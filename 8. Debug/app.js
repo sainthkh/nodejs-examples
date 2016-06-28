30ed6e7a6fdd46a2bf985e5ab694c08756b5d32e
sainthkh <sainthkh@naver.com>
2016-06-28 16:01:45 +0900

    Added debug example.

---
+++ 8. Debug/app.js
@@ -0,0 +1,24 @@
+var http = require('http');
+debugger;
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  debugger;
+  res.end('Hello World\n');
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
+
+/*
+Commands
+
+debugger - stop there.
+
+cont, c - Continue execution
+next, n - Step next
+step, s - Step in
+out, o - Step out
+
+setBreakpoint()
+repl - interaction. 
+
+More info: https://nodejs.org/api/debugger.html
+*/
