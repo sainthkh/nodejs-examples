2b0efe50cc9405e2b8c59d7fbaf213db79f669df
sainthkh <sainthkh@naver.com>
2016-06-29 12:11:17 +0900

    Added intall instructions.

---
+++ 2. Express.js/install.js
@@ -0,0 +1,6 @@
+//$ mkdir myapp
+//$ cd myapp
+//$ npm init
+//entry point: (index.js) -> app.js
+//$ npm install express --save
+//Node modules installed with the --save option are added to the dependencies list in the package.json file. Afterwards, running npm install in the app directory will automatically install modules in the dependencies list.
